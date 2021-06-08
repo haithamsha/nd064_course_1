7599bf103df6ae43722d94ca4f17ddf7d015155e
Haitham Shaaban AbdElFatah AbdElRady <shahaitham@gmai.com>
2021-06-08 17:10:17 +0200

    done Exercise: Endpoints for Application Status

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,32 @@
 from flask import Flask
+from flask import json
+from werkzeug.wrappers import response
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! haitham"
+
+
+@app.route("/status")
+def health_check():
+    res = app.response_class(
+        response = json.dumps({"result": "Ok - healthy"}),
+        status = 200,
+        mimetype = 'application/json'
+    )
+    return res
+
+@app.route("/metrics")
+def metrics():
+    res = app.response_class(
+        response = json.dumps({"status": "success", "code": 0, "data": "UserCount: 140, UserCountActive: 23"}),
+        status = 200,
+        mimetype = 'application/json'
+    )
+    return res
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
+ 
